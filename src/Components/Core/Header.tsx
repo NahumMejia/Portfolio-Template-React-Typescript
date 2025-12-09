d23e2f155952de1f2a3de1266134db0a53a77ce8
Nahum <nahummejiad@gmail.com>
2025-12-08 20:23:19 -0600

    Dexter

--- src/Components/Core/Header.tsx
+++ src/Components/Core/Header.tsx
@@ -5,8 +5,8 @@ export default function Header() {
         <nav className="top-0 z-50 fixed bg-black shadow-blue-900 shadow-lg px-4 py-6 w-full">
             <div className="flex justify-between items-center mx-auto px-2 max-w-7xl">
                 <span className="flex font-ghotic text-white text-2xl">
-                <span className="text-blue-200">Nahum</span>
-                <span className="ml-1 text-blue-100">Mejia</span>
+                <span className="text-blue-200">Dexter</span>
+                <span className="ml-1 text-blue-100">Morgan</span>
                 </span>
             </div>
         </nav>
